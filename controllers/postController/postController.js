3f2fb6b0446aa45579f952deedaa987f38ee5d36
Your Name <tesfayegirma360@gmail.com>
2022-09-07 17:46:54 +0300

    CRUD Post

---
+++ controllers/postController/postController.js
@@ -0,0 +1,114 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+export const readPost = async (req, res) => {
+
+    try {
+        const post = await prisma.post.findMany({
+            include: {
+                author: true
+            }
+        })
+        res.json({
+            status: 'success',
+            data: post
+        })
+
+    } catch (error) {
+        res.json({
+            status: 'error',
+            data: null
+        })
+    }
+}
+
+export const updatePost = async (req, res) => {
+
+    try {
+        const { id } = req.params
+        const { title, content, published } = req.body
+
+        const post = await prisma.post.update({
+            where: {
+                id
+            },
+            data: {
+                title,
+                content,
+                published
+            },
+            include: {
+                author: true
+            }
+
+        })
+        res.json({
+            status: "success",
+            data: post
+        })
+    } catch (error) {
+        res.json({
+            status: error,
+            data: null
+        })
+    }
+
+}
+
+
+export const createPost = async (req, res) => {
+    try {
+        const { title, content, published, author: { id }
+        } = req.body;
+
+        const newPost = await prisma.post.create({
+            data: {
+                title,
+                content,
+                published,
+                author: {
+                    connect: {
+                        id
+                    }
+                }
+            },
+            include: {
+                author: true
+            }
+        });
+        res.send({
+            status: 'success',
+            data: newPost
+        });
+    } catch (error) {
+        res.send({
+            status: 'error',
+            message: error.message
+        }
+        );
+    }
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const deletePost = await prisma.post.update({
+            where: {
+                id
+            }
+        })
+        res.json({
+            status: "success",
+            message: deletePost
+        })
+
+    } catch (error) {
+        res.json({
+            status: "error",
+            data: error
+        })
+    }
+}
--- router/router.js
+++ router/router.js
@@ -1,19 +1,31 @@
 import express from 'express';
+
 import createUser from '../controllers/userController/createOperation.js'
 import readOperation from '../controllers/userController/readOperation.js'
 import deleteOperation from '../controllers/userController/deleteOperation.js'
 import updateOperation from '../controllers/userController/updateOperation.js'
 
+import {
+    createPost,
+    readPost,
+    updatePost,
+    deletePost
+} from '../controllers/postController/postController.js'
+
+
 const router = express.Router();
 
 
 router.post('/createUser', createUser)
-
 router.get('/users', readOperation)
-
 router.delete('/deleteUser/:id', deleteOperation)
-
 router.put('/updateUser/:id', updateOperation)
+
+//POST CONTENT ROUTE
+router.post('/createPost', createPost)
+router.get('/posts', readPost)
+router.put('/updatePost/:id', updatePost)
+router.delete('/deletePost/:id', deletePost)
 
 
 export default router
